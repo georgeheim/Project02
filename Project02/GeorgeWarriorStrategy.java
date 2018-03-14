98c3ac4a5cc51ad8c20d76315504447fc16b1167
George Heim <stglh03@moravian.edu>
2018-03-14 19:08:20 -0400

    Modified George's strategies

--- Project02/GeorgeWarriorStrategy.java
+++ Project02/GeorgeWarriorStrategy.java
@@ -4,24 +4,31 @@ import Project02.People;
 
 public class GeorgeWarriorStrategy implements EncounterStrategy {
 
-    public int strategy(People me, People otherPerson){
+    public int strategy(People me, People otherPerson) {
       int lifePoints = 0;
-      int points = me.getLifePoints() - otherPerson.getLifePoints();
-      if (me.getNation() != otherPerson.getNation()) { // If from another nation
-          if (me.getType() == otherPerson.getType()) { // If warrior
-              if (points > 0) { // If I have more health
-                  lifePoints = otherPerson.getLifePoints()/2;
+      int difference = me.getLifePoints() - otherPerson.getLifePoints();
+      if (me.getNation() != otherPerson.getNation()) { // Is from another nation
+          if (me.getType() == otherPerson.getType()) { // Is a warrior
+              if (difference > 0) { // I have more health
+                  lifePoints = otherPerson.getLifePoints();
               }
-              else { // If they have more health
+              else { // They have more health
                   lifePoints = me.getLifePoints()/2;
               }
           }
-          else { // If wizard
-              if (points > 0) { // If I have more health
+          else { // Is a wizard
+              if (difference > 0) { // I have more health
                   lifePoints = otherPerson.getLifePoints();
               }
-              else { // If they have more health
-                  lifePoints = me.getLifePoints();
+              else { // They have more health
+                  lifePoints = me.getLifePoints()/4;
+              }
+          }
+      }
+      else { // Is from my nation
+          if (difference > 0) { // I have more health
+              if (me.getTribe() == otherPerson.getTribe()) { // Is from my tribe
+                  lifePoints = -(difference)/2; // Balance life totals
               }
           }
       }
