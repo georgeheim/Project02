8c2248bc0f117fcab1b23ab9cda7d24316eb493f
George Heim <stglh03@moravian.edu>
2018-03-28 15:31:09 -0400

    Corrected mistakes in GeorgeDisplay

--- Project02/GeorgeDisplay.java
+++ Project02/GeorgeDisplay.java
@@ -8,6 +8,7 @@ public class GeorgeDisplay implements Observer {
     private ArrayList<People> worldCreatedPeople;
     private World earth;
     private int numberOfRounds;
+    private int roundNumber;
 
     public GeorgeDisplay(World earth) {
         this.earth = earth;
@@ -21,17 +22,18 @@ public class GeorgeDisplay implements Observer {
     }
 
     public void display() {
-        System.out.println("Round " + numberOfRounds);
-        System.out.println("The current survivors from GFNation are: ");
+	roundNumber++;
+        System.out.println("Round " + roundNumber);
+        System.out.println("The current survivors from George's tribe are: ");
         int peopleCount = 0;
         for(Integer i = 0; i < worldCreatedPeople.size(); i++){
-            if(worldCreatedPeople.get(i).getNation().equals("GFNation")) {
+            if(worldCreatedPeople.get(i).getTribe().equals("GeorgeTribe")) {
                 if(worldCreatedPeople.get(i).getLifePoints() > 0) {
                     System.out.println(worldCreatedPeople.get(i));
                     peopleCount++;
                 }
             }
         }
-        System.out.println("GFNation has " + peopleCount + " people left");
+        System.out.println("George's tribe has " + peopleCount + " people left");
     }
 }
